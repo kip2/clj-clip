2d7dd644fe5ee101a2c7bfd53798dead134cc6c8
kip2 <tqa1no4@gmail.com>
2024-11-09 20:42:42 +0900

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
-(defproject clj-clip "0.1.0-SNAPSHOT"
+(defproject com.github.kip2/clj-clip "0.1.0"
   :description "A simple library for handling the clipboard within the Clojure REPL."
   :url "https://github.com/kip2/clj-clip"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]]
-  :repl-options {:init-ns clj-clip.core})
+  :repl-options {:init-ns clj-clip.core}
+  :deploy-repositories [["clojars" {:url "https://repo.clojars.org"}]])
+
